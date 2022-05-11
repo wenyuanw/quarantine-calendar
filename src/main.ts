6e6150691b2758927dfb6555aa0ecefd92d2be0f
wenyuan <wenyuan19@163.com>
2022-05-11 17:52:37 +0800

    fix: 模块导入问题

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import 'v-calendar/dist/style.css'
-import { SetupCalendar } from 'v-calendar'
+import SetupCalendar from 'v-calendar'
 
 createApp(App)
   .use(SetupCalendar)
